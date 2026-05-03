359ce1c4cb167e0b5e2de974b8dda00a79ace3aa
Akarsh-Coding <akarsh4.pvt@gmail.com>
2026-05-03 07:06:49 +0530

    feat: implement homepage-specific components; common components yet to be completed

---
+++ frontend/src/landing_page/home/Pricing.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+function Pricing() {
+    return (
+        <div className="container"><div className="row ">
+            <div className="col-4">
+                <h1 className='mb-4 fs-2'>Unbeatable pricing</h1>
+                <p>We pioneered the concept of discount broking and price transparency in India. Flat fees and no hidden charges.</p>
+                <a href="#">See Pricing <i class="fa-solid fa-right-long"></i></a>
+            </div>
+            <div className="col-2"></div>
+            <div className="col-6"><div className="row text-center mb-5">
+                <div className="col border p-4 d-flex flex-column justify-content-center">
+                    <h1 className='mb-3'>₹0</h1>
+                    <p>Free equity delivery and <br /> direct mutual funds</p>
+                </div>
+                <div className="col border p-4 d-flex flex-column justify-content-center ">
+                    <h1 className='mb-3 '>₹20</h1>
+                    <p>Intraday and F&O</p>
+                </div>
+                </div></div>
+            </div></div>
+    );
+}
+
+export default Pricing;
